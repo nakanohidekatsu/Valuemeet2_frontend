3a1948e4bfa5ca590c0a3bf698a2b86b078133b9
Kyoko Nakamura <kyoko.nakamura.2021@globis.ac.jp>
2025-08-06 17:09:43 +0900

    Add standalone output config for Azure deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone', // ←これを追加
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   images: { unoptimized: true },
+// };
+
+// module.exports = nextConfig;
